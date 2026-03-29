559a643a110176d9747e8dcc3f30e48939a59f19
Michael Cirkl <cirklmic@cvut.cz>
2026-03-29 10:58:15 +0200

    javadocs for typed exceptions

--- blob-storage-adapter/src/main/java/michaelcirkl/ubsa/client/exception/types/ConditionFailedException.java
+++ blob-storage-adapter/src/main/java/michaelcirkl/ubsa/client/exception/types/ConditionFailedException.java
@@ -2,6 +2,17 @@ package michaelcirkl.ubsa.client.exception.types;
 
 import michaelcirkl.ubsa.client.exception.UbsaException;
 
+/**
+ * Thrown when a conditional request fails.
+ *
+ * <p>UBSA maps the following provider error codes to this exception:
+ * <ul>
+ *   <li>Azure: {@code ConditionNotMet}, {@code AppendPositionConditionNotMet},
+ *   {@code MaxBlobSizeConditionNotMet}, {@code SequenceNumberConditionNotMet},
+ *   {@code SourceConditionNotMet}, {@code TargetConditionNotMet}</li>
+ *   <li>AWS: {@code PreconditionFailed}</li>
+ * </ul>
+ */
 public class ConditionFailedException extends UbsaException {
     public ConditionFailedException(String message, Throwable nativeException, int statusCode) {
         super(message, nativeException, statusCode);
--- blob-storage-adapter/src/main/java/michaelcirkl/ubsa/client/exception/types/RequestTimeoutException.java
+++ blob-storage-adapter/src/main/java/michaelcirkl/ubsa/client/exception/types/RequestTimeoutException.java
@@ -2,6 +2,15 @@ package michaelcirkl.ubsa.client.exception.types;
 
 import michaelcirkl.ubsa.client.exception.UbsaException;
 
+/**
+ * Thrown when the provider times out while processing the request.
+ *
+ * <p>UBSA maps the following provider error codes to this exception:
+ * <ul>
+ *   <li>Azure: {@code OperationTimedOut}</li>
+ *   <li>AWS: {@code RequestTimeout}</li>
+ * </ul>
+ */
 public class RequestTimeoutException extends UbsaException {
     public RequestTimeoutException(String message, Throwable nativeException, int statusCode) {
         super(message, nativeException, statusCode);
